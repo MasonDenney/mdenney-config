911bffcd5ff1d2957b67986f38fd19fc71cf60f9
MasonDenney <masondenney@gmail.com>
2024-03-03 13:48:26 -0500

    working and cleanup

--- setup.sh
+++ setup.sh
@@ -1,13 +1,21 @@
-#terraform apply
-#gcloud container clusters get-credentials mygke-cluster-prod --region us-east1
+#make k8s cluster
+terraform -chdir tf apply
+
+#configure kubectl
+gcloud container clusters get-credentials mygke-cluster-prod --region us-east1
+
+# Create ArgoCD
 kubectl create namespace argocd
-kustomize build applications/infra/argocd/do --enable-helm | kubectl apply -f -
+kustomize build k8s/applications/argocd --enable-helm | kubectl apply -f -
 kubens argocd
-kustomize build meta/do --enable-helm | kubectl apply -f -
-#Warning: metadata.finalizers: "resources-finalizer.argocd.argoproj.io": prefer a domain-qualified finalizer name to avoid accidental conflicts with other finalizer writers
-#Error from server (BadRequest): error when creating "STDIN": AppProject in version "v1alpha1" cannot be handled as a AppProject: strict decoding error: unknown field "spec.namespaceResourceBlacklist[2].orphanedResources", unknown field "spec.namespaceResourceBlacklist[2].roles"
-#IN ARGOCD - InvalidSpecError Application referencing project guestbook which does not exist
 
 # UI
-#kubectl port-forward svc/argocd-server -n argocd 8080:443
+argocd admin initial-password -n argocd
 #kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+kubectl port-forward svc/argocd-server -n argocd 8080:443
+
+# Create App of Apps
+kubectl apply -f application-applications.yaml
+
+# To add new apps make new subfolder under k8s and then add new entry in k8s/applications/kustomization.yaml
+# This will use the argocd-apps helm chart to generate Application files using the template from the helm-charts/argocd-app
