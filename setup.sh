bc65f4993bf6a58a7b0d42403065a69d9354001a
MasonDenney <masondenney@gmail.com>
2024-03-03 13:50:56 -0500

    rename

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ argocd admin initial-password -n argocd
 kubectl port-forward svc/argocd-server -n argocd 8080:443
 
 # Create App of Apps
-kubectl apply -f application-applications.yaml
+kubectl apply -f appofapps.yaml
 
 # To add new apps make new subfolder under k8s and then add new entry in k8s/applications/kustomization.yaml
 # This will use the argocd-apps helm chart to generate Application files using the template from the helm-charts/argocd-app
