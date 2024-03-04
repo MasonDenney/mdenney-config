407f6ce542e0441a4f176c6445db12ef964dde24
MasonDenney <masondenney@gmail.com>
2024-03-04 08:00:45 -0500

    add destroy command

--- setup.sh
+++ setup.sh
@@ -23,3 +23,6 @@ argocd admin initial-password -n argocd
 #kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 kubectl port-forward svc/argocd-server -n argocd 8080:443
 # In browser, open https://localhost:8080
+
+# When ready to tear down and clean up run
+#terraform -chdir=tf destroy -auto-approve
